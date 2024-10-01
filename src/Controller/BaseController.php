249e237f4b269155363510b42b096a821dd65d6a
Killian237 <killian.henriet@ecoles-epsi.net>
2024-10-01 14:03:02 +0200

    finalisation

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Form\AjoutBonbonType;
 
 class BaseController extends AbstractController
 {
@@ -12,6 +13,16 @@ class BaseController extends AbstractController
     public function index(): Response
     {
         return $this->render('base/index.html.twig', [
+        ]);
+    }
+
+    #[Route('/ajoutBonbon', name: 'app_ajoutBonbon')]
+    public function ajoutBonbon(): Response
+    {
+        $form = $this->createForm(AjoutBonbonType::class);
+        return $this->render('base/ajoutBonbon.html.twig', [
+            'form' => $form->createView()
+           
         ]);
     }
 }
